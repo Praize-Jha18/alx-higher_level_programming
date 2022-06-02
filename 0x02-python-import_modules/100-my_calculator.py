aa1230d7a374e59fafe505be4c0d9c49e74963ed
Praize-Jha18 <praisejahfrancis@gmail.com>
2022-06-02 16:45:48 -0700

    Correction of advanced task 6 and eight

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,21 +2,21 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    num_args = len(sys.argv)
-    if (num_args != 4):
+    argc = len(sys.argv)
+    if (argc != 4):
         print("Usage: {:s} <a> <operator> <b>".format(sys.argv[0]))
         exit(1)
     else:
         a = int(sys.argv[1])
         op = sys.argv[2]
         b = int(sys.argv[3]
-    if op == '+':
+    if (op) == '+':
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    elif op == '-':
+    elif (op) == '-':
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    elif op == '*':
+    elif (op) == '*':
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    elif op == '/':
+    elif (op) == '/':
     print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
