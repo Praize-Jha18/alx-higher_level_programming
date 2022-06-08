244fa32326bb3eee1ef341f7df7cd3dad7551735
Praize-Jha18 <praisejahfrancis@gmail.com>
2022-06-08 16:56:28 -0700

    Python Data Structures: Sets,Dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if (a_dictionary == {} or a_dictionary is None):
         return None
-    return max(a_dictionary)
+    else:
+        max_value = max(a_dictionary, key=a_dictionary.get)
+        return (max_value)
